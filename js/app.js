0c01ee2df1c3d68f1b4b09211a62045902bef922
Kaique Bazil <107780135+kaiquebazil@users.noreply.github.com>
2025-06-06 14:08:47 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1893,3 +1893,15 @@ function importData(event) {
     reader.readAsText(file);
 }
 
+/*************************** */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('Service Worker registered:', registration);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
